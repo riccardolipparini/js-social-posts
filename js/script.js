94a04ce322e1b80f3eed124bd13df6ea048854b6
Riccardo <riccardolipparini0@gmail.com>
2021-11-05 16:00:05 +0100

    post stampati

---
+++ js/script.js
@@ -0,0 +1,162 @@
+// Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro javascript in cui:
+// - Creiamo il nostro array di oggetti che rappresentano ciascun post.
+// Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+// nome autore,
+// foto profilo,
+// data,
+// testo del post,
+// immagine (non tutti i post devono avere una immagine) [quindi gestisco il caso],
+// numero di likes.
+// - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
+// - Rendiamo il tasto “Mi Piace” cliccabile con incremento del counter dei likes.
+
+const stamp = document.getElementById("container");
+
+const posts = [
+    {
+        "profilePic" : "https://unsplash.it/300/300?image=15",
+        "name": "Phil Mangione",
+        "date" : "05/11/2021",
+        "postText" : "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "postImage" : "https://unsplash.it/600/300?image=171"    
+    },
+    {
+        "profilePic" : "https://picsum.photos/300/300",
+        "name": "Paolo Rossi",
+        "date" : "04/11/2021",
+        "postText" : "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto.Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "postImage" : ""    
+    },
+    {
+        "profilePic" : "https://picsum.photos/300/300",
+        "name": "Antonio Grismaldi",
+        "date" : "02/11/2546",
+        "postText" : "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "postImage" : ""    
+    },
+    {
+        "profilePic" : "https://picsum.photos/200/300",
+        "name": "Lucia Borbone",
+        "date" : "01/11/1670",
+        "postText" : "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "postImage" : "https://picsum.photos/600/300"    
+    }
+    
+    
+]
+console.log(posts);
+
+stampPost();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function stampPost(){
+    for(i = 0; i < posts.length; i++){
+
+        const oggetto = posts[i];
+        const profilePic = oggetto.profilePic;
+        const name = oggetto.name;
+        const date = oggetto.date;
+        const postText = oggetto.postText;
+        const postImage = oggetto. postImage;
+
+        document.getElementById("container").innerHTML +=`<div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${profilePic}" alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${name}</div>
+                    <div class="post-meta__time">${date}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${postText}</div>
+        <div class="post__image">
+            <img src="${postImage}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">Phil Mangione</div>
+                        <div class="post-meta__time">4 mesi fa</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__image">
+                <img src="https://unsplash.it/600/300?image=171" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div> */
